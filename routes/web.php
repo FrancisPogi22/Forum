b96910889a798722b2fdd067b4ad9d641e83f215
Francis Cabusas <117327118+FrancisPogi22@users.noreply.github.com>
2024-05-03 20:24:06 +0800

    Update Middleware

--- routes/web.php
+++ routes/web.php
@@ -8,22 +8,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
-Route::post('/create', function () {
-    return view('posts/create');
-})->name('create');
-Route::post('/store', [PostController::class, 'store'])->name('store');
-Route::get('/edit/{id}', [PostController::class, 'edit'])->name('edit');
-Route::patch('/update/{id}', [PostController::class, 'update'])->name('update');
-Route::delete('/destroy/{id}', [PostController::class, 'destroy'])->name('destroy');
-
 Route::get('/register', function () {
     return view('userpages/register');
 })->name('register');
-
 Route::post('/register', [RegisterController::class, 'register'])->name('register.submit');
-
 Route::controller(AuthenticationController::class)->group(function () {
     Route::post('/login', 'login')->name('login.user');
-    Route::get('/logout', 'logout')->name('logout.user');
 });
 
+Route::middleware(['auth'])->group(function () {
+    Route::post('/create', function () {
+        return view('posts/create');
+    })->name('create');
+    Route::post('/store', [PostController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [PostController::class, 'edit'])->name('edit');
+    Route::patch('/update/{id}', [PostController::class, 'update'])->name('update');
+    Route::delete('/destroy/{id}', [PostController::class, 'destroy'])->name('destroy');
+
+    Route::controller(AuthenticationController::class)->group(function () {
+        Route::get('/logout', 'logout')->name('logout.user');
+    });
+});
