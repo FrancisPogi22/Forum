8e558a78e63c01fe4a9e4e4f662d5c4480e43e21
Abbygaile Blue Calitis <calitisabby@gmail.com>
2024-05-03 19:51:27 +0800

    route for registration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,3 +15,14 @@ Route::post('/store', [PostController::class, 'store'])->name('store');
 Route::get('/edit/{id}', [PostController::class, 'edit'])->name('edit');
 Route::patch('/update/{id}', [PostController::class, 'update'])->name('update');
 Route::delete('/destroy/{id}', [PostController::class, 'destroy'])->name('destroy');
+
+Route::get('/register', function () {
+    return view('userpages/register');
+})->name('register');
+
+Route::post('/register', [RegisterController::class, 'register'])->name('register.submit');
+
+Route::controller(AuthenticationController::class)->group(function () {
+    Route::post('/login', 'login')->name('login.user');
+    Route::get('/logout', 'logout')->name('logout.user');
+});
